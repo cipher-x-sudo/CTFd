4c895dcc509b820c8f2c565aeea06a9b0bba92d0
C1PH3R | $UD0 FUZz3RS <119114621+cipher-x-sudo@users.noreply.github.com>
2026-02-06 02:53:14 +0500

    chore: update plugin blacklist to exclude 'containers' directory

--- CTFd/plugins/__init__.py
+++ CTFd/plugins/__init__.py
@@ -181,7 +181,7 @@ def bypass_csrf_protection(f):
 
 def get_plugin_names():
     modules = sorted(glob.glob(app.plugins_dir + "/*"))
-    blacklist = {"__pycache__"}
+    blacklist = {"__pycache__", "containers"}
     plugins = []
     for module in modules:
         module_name = os.path.basename(module)
